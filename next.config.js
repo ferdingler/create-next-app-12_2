4fd7d62a3f6fb5f61233e20b6cb33b0635f62753
Fernando Dingler <fdingler@amazon.com>
2022-09-02 12:33:53 -0700

    Added custom headers

--- next.config.js
+++ next.config.js
@@ -2,6 +2,23 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'x-custom-header',
+            value: 'my custom header value',
+          },
+          {
+            key: 'x-another-custom-header',
+            value: 'my other custom header value',
+          },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
